28f369211e5d5f732ea98a4d96f54f4bbf254428
clemente7sftw <gabiclemente2007@gmail.com>
2025-08-26 15:39:33 -0300

    Adicionando Edit de Email e Senha na Tela de Login, Modificando Icone

--- Login.pas
+++ Login.pas
@@ -4,16 +4,26 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage,
+  Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
     FotoLogin: TImage;
+    LadoEsquerdo: TPanel;
+    Image1: TImage;
+    EdEmail: TEdit;
+    EdSenha: TEdit;
+    LbEmail: TLabel;
+    LbSenha: TLabel;
+    procedure EdEmailKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+
 
   private
     { Private declarations }
   public
-    { Public declarations }
+
   end;
 
 var
@@ -24,5 +34,13 @@ implementation
 {$R *.dfm}
 
 
+
+
+procedure TForm1.EdEmailKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+if Key = VK_RETURN then
+   perform(WM_NEXTDLGCTL,0,0);
+end;
 
 end.
