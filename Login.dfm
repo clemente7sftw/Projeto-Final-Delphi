0fdeb4226107b036e34f22f9e149f4f77cab3443
clemente7sftw <gabiclemente2007@gmail.com>
2025-08-26 14:58:06 -0300

    Adicionando a Página de Login

---
+++ Login.dfm
@@ -0,0 +1,20 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Login BeautyStage'
+  ClientHeight = 1061
+  ClientWidth = 1924
+  Color = 1186595
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object FotoLogin: TImage
+    Left = 1008
+    Top = 16
+    Width = 945
+    Height = 1100
+  end
+end
---
+++ Login.pas
@@ -0,0 +1,28 @@
+unit Login;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+
+type
+  TForm1 = class(TForm)
+    FotoLogin: TImage;
+
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+
+
+end.
