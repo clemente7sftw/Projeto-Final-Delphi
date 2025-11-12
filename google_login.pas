bbd71fb778a951ea49fe7c54d1664dfbd10466a0
clemente7sftw <gabiclemente2007@gmail.com>
2025-11-11 21:26:41 -0300

    Tentando Adicionar Login com Google

---
+++ google_login.pas
@@ -0,0 +1,25 @@
+unit google_login;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.OleCtrls, SHDocVw;
+
+type
+  TForm33 = class(TForm)
+    WebBrowser1: TWebBrowser;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form33: TForm33;
+
+implementation
+
+{$R *.dfm}
+
+end.
